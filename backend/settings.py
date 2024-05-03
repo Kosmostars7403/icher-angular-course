a167123dcd0d88d1c5438116d6b32b008eb1ba58
Herrifly <nani-4545@mail.ru>
2024-05-03 15:31:44 +0300

    init backend

---
+++ backend/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+
+    SECRET_KEY_JWT: str
+    SECRET_AUTH: str
+
+    class Config:
+        env_file = "../.env"
+
+
+settings = Settings()
